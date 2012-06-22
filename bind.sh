6dc8bda0bbb4a11e8ac400ed1c1d77ab9cd4791a
psachin <iclcoolster@gmail.com>
2012-06-22 14:51:41 +0530

    Subject: exit status for bind.sh
    Description:

--- bind.sh
+++ bind.sh
@@ -1,37 +1,52 @@
 #!/system/bin/sh
 
+export bin=/system/bin
+export PATH=$bin:/usr/bin:/usr/sbin:/bin:$PATH
+export TERM=linux
+export HOME=/root
+export MNT=/data/local/linux
+
 export SDCARD=/mnt/sdcard/APL
 export WWW=/data/local/linux/var/www/html
 
-while true;
-    do 
-        if [ -n "$(pgrep com.aakash.lab)" ] #True if length of string is non zero
-            then
-                if [ ! -d ${SDCARD} ] || [ ! -d ${WWW} ]
-                    then
-                        mkdir -p ${SDCARD}/c/code 
-                        mkdir -p ${WWW}/c/code
+FLAG=1
 
-                        mkdir -p ${SDCARD}/cpp/code 
-                        mkdir -p ${WWW}/cpp/code
+while [ $FLAG -eq 1 ]
+do
     
-                        mkdir -p ${SDCARD}/python/code 
-                        mkdir -p ${WWW}/python/code
-    
-                        mkdir -p ${SDCARD}/scilab/code 
-                        mkdir -p ${WWW}/scilab/code
-                        
-                        mkdir -p ${SDCARD}/scilab/image 
-                        mkdir -p ${WWW}/scilab/image
-                fi
-            busybox mount -o bind ${SDCARD}/c/code ${WWW}/c/code
-            busybox mount -o bind ${SDCARD}/cpp/code ${WWW}/cpp/code
-            busybox mount -o bind ${SDCARD}/python/code ${WWW}/python/code
-            busybox mount -o bind ${SDCARD}/scilab/code ${WWW}/scilab/code
-            busybox mount -o bind ${SDCARD}/scilab/image ${WWW}/scilab/image
-            busybox  chroot  $MNT /bin/bash -c "nohup python /root/rsync.py &>'/dev/null'&"
-            sleep 2 
-            exit
-        fi
-        sleep 5
-    done   
+    while [ $(pgrep com.aakash.lab | wc -l) -eq 1 ]
+    do
+	
+	if [ ! -d ${SDCARD} ] && [ ! -d ${WWW} ]
+	then                                                                  
+	    mkdir -p ${SDCARD}/c/code                                         
+	    mkdir -p ${WWW}/c/code                                            
+	    
+	    mkdir -p ${SDCARD}/cpp/code                                       
+	    mkdir -p ${WWW}/cpp/code                                          
+	    
+	    mkdir -p ${SDCARD}/python/code                                     
+	    mkdir -p ${WWW}/python/code     
+	    
+	    mkdir -p ${SDCARD}/scilab/code
+	    mkdir -p ${WWW}/scilab/code   
+	    
+	    mkdir -p ${SDCARD}/scilab/image
+	    mkdir -p ${WWW}/scilab/image   
+	fi
+
+	busybox mount -o bind ${SDCARD}/c/code ${WWW}/c/code
+	busybox mount -o bind ${SDCARD}/cpp/code ${WWW}/cpp/code
+	busybox mount -o bind ${SDCARD}/python/code ${WWW}/python/code
+	busybox mount -o bind ${SDCARD}/scilab/code ${WWW}/scilab/code
+	busybox mount -o bind ${SDCARD}/scilab/image ${WWW}/scilab/image
+
+	busybox  chroot  /data/local/linux /bin/bash -c "nohup python /root/rsync.py &>'/dev/null'&"
+
+	FLAG=0
+	exit 0 
+    done
+done
+
+exit 0
+
