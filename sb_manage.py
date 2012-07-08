71bb209471c236b054fba9d98d157a68ce87f8a9
Srikant <usp71084@gmail.com>
2012-07-08 12:16:44 +0530

    now scilab gui mode is controlled by scilab.cgi, removed code from here

--- sb_manage.py
+++ sb_manage.py
@@ -24,32 +24,30 @@ allPaths = ['/tmp/cbin',
             '/tmp/cperror',
             '/tmp/1.py',
             '/var/www/html/scilab/tmp/1.cde',
-            '/var/www/html/scilab/tmp/plot.cde']
+            '/var/www/html/scilab/tmp/1.err']
 
 
 def returnCommand():
     for checkPath in allPaths:
-        sleep(1)
+        sleep(0.6)
         if(path.isfile(checkPath)):
             if((checkPath == allPaths[0]) or (checkPath == allPaths[2])):
                 command = commonCommand + '%s' %(checkPath)
                 #used -w option in c and cpp to avoid warnings
                 break
-            elif(((checkPath == allPaths[1] or checkPath == allPaths[3])) and (path.getsize(checkPath))):
+            #elif(((checkPath == allPaths[1] or checkPath == allPaths[3])) and (path.getsize(checkPath))):
+            elif(((checkPath == allPaths[1] or checkPath == allPaths[3] or checkPath == allPaths[6])) and (path.getsize(checkPath))):
                 command = commonCommand + "'cat %s'" %(checkPath)
                 break 
             elif(checkPath == allPaths[4]):
                 command = commonCommand + "'python %s'" %(checkPath)
                 break
             elif(checkPath == allPaths[5]):
-                system("export DISPLAY=:0")
                 command = commonCommand + "'scilab -nogui -nb -f %s'" %(checkPath)
                 break
-            elif(checkPath == allPaths[6]):
-                system("export DISPLAY=:1")
-                #command = commonCommand + "'scilab -nb -f %s'" %(checkPath)
-                command = 'scilab -nb -f %s' %(checkPath)
-                break
+#            elif(checkPath == allPaths[6]):
+ #               command = commonCommand + "'cat %s'" %(checkPath)
+  #              break
         else:
             command = ''
             checkPath = '/root/sb_manage.py'
@@ -60,21 +58,16 @@ def executeCommand():
     if (previousTimeStamp[0] == 1.0) and (pathAvailable not in allPaths):
         system("killall -s 9 shellinaboxd")
         blankCommand = 'shellinaboxd --localhost-only -t -s /:www-data:www-data:/:true'
-#        sleep(0.4)
         Popen(blankCommand,shell=True, stdout=PIPE)
         print "I am in first time execution",previousTimeStamp
-        #break
+
     elif((previousTimeStamp[0] != path.getmtime(pathAvailable)) and (pathAvailable in allPaths)):
         system("killall -s 9 shellinaboxd")
- #       sleep(0.4)
         Popen(SBcommand,shell=True, stdout=PIPE)
-#        if pathAvailable is allPaths[6]:
- #           sleep(3)
         system("touch /var/www/html/flag")
         system("chown www-data.www-data /var/www/html/flag")
         print SBcommand, previousTimeStamp
         previousTimeStamp[0] = path.getmtime(pathAvailable)
-        #break    
 
 
 while True:
