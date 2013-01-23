878e9da074a68692dd5bb5a4f8b2e3f98b4ce8b4
psachin <iclcoolster@gmail.com>
2013-01-23 17:04:07 +0530

    Subject: files synced with APL-apk
    Description:
    
    - APL.apk
    - preinstall.sh
    - aakash.sh

--- aakash.sh
+++ aakash.sh
@@ -1,116 +1,166 @@
 #!/system/bin/sh
 
 export bin=/system/bin
-export PATH=$bin:/usr/bin:/usr/sbin:/bin:$PATH
+export PATH=$bin:/usr/bin:/usr/sbin:/bin:/usr/local/bin:$PATH
 export TERM=linux
 export HOME=/root
+
+### aakash programming lab
 export MNT=/data/local/linux
 export EG=/data/example
 export SDCARD=/mnt/sdcard/APL
 export WWW=/data/local/linux/var/www/html
 
-FLAG=1
-SD=1
+### gkaakash 
+export MNTG=/data/local/gkaakash
 
-while [ $FLAG -eq 1 ]
+### ipython
+export MNTI=/data/local/ipy
 
-do
+APL=1
+GK=1
+IPY=1
 
-            function startServices 
-            {
-        	    # mounting essential file systems to chroot
-                busybox mount -t proc proc $MNT/proc
-        	    busybox mount -o bind /dev $MNT/dev
-            	busybox mount -t sysfs sysfs $MNT/sys
-                busybox  chroot  $MNT /bin/bash -c "mount /dev/pts" 
+function startServicesAPL
+    {
+        # mounting essential file systems to chroot
+        busybox mount -t proc proc $MNT/proc
+        busybox mount -o bind /dev $MNT/dev
+        busybox mount -t sysfs sysfs $MNT/sys
+        busybox  chroot  $MNT /bin/bash -c "mount /dev/pts" 
+        busybox  chroot  $MNT /bin/bash -c "chown -R www-data.www-data /var/www/"
+        busybox  chroot  $MNT /bin/bash -c "chmod -R a+x /usr/lib/cgi-bin"
 
-        	    busybox  chroot  $MNT /bin/bash -c "chown -R www-data.www-data /var/www/"
-            	busybox  chroot  $MNT /bin/bash -c "chmod -R a+x /usr/lib/cgi-bin"
-                
-                # cleaning and starting apache 
-        	    busybox  chroot  $MNT /bin/bash -c "rm /dev/null"
-            	busybox  chroot  $MNT /bin/bash -c "rm /var/run/apache2/*"
-            	busybox  chroot  $MNT /bin/bash -c "rm /var/run/apache2.pid"
-            	busybox  chroot  $MNT /bin/bash -c "service apache2 stop"
-        	    busybox  chroot  $MNT /bin/bash -c "service apache2 start"
+        # cleaning and starting apache 
+        busybox  chroot  $MNT /bin/bash -c "rm /dev/null"
+        busybox  chroot  $MNT /bin/bash -c "rm /var/run/apache2/*"
+        busybox  chroot  $MNT /bin/bash -c "rm /var/run/apache2.pid"
+        busybox  chroot  $MNT /bin/bash -c "service apache2 stop"
+        busybox  chroot  $MNT /bin/bash -c "service apache2 start"
 
-                busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/c/exbind/.open_file.c"
-                busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/cpp/exbind/.open_file.cpp"
-                busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/python/exbind/.open_file.py"
-                busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/scilab/exbind/.open_file.cde"
-        	    
-                # running shellinaboxd, sb_manage and Xvfb
-            	busybox  chroot  $MNT /bin/bash -c "shellinaboxd --localhost-only -t -s /:www-data:www-data:/:true &"
-        	    busybox  chroot  $MNT /bin/bash -c "rm /tmp/.X0-*" 
-            	busybox  chroot  $MNT /bin/bash -c "nohup Xvfb :0 -screen 0 640x480x24 -ac < /dev/null > Xvfb.out 2> Xvfb.err &"
-            }
+        busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/c/exbind/.open_file.c"
+        busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/cpp/exbind/.open_file.cpp"
+        busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/python/exbind/.open_file.py"
+        busybox  chroot  $MNT /bin/bash -c "chmod 777 /var/www/html/scilab/exbind/.open_file.cde"
+        
+        # running shellinaboxd, sb_manage and Xvfb
+        busybox  chroot  $MNT /bin/bash -c "shellinaboxd --localhost-only -t -s /:www-data:www-data:/:true &"
+        busybox  chroot  $MNT /bin/bash -c "rm /tmp/.X0-*" 
+        busybox  chroot  $MNT /bin/bash -c "nohup Xvfb :0 -screen 0 640x480x24 -ac < /dev/null > Xvfb.out 2> Xvfb.err &"
 
-            busybox mkdir -p $MNT
-
-            if [ -f /mnt/sdcard/apl.img ] && [ $SD -eq 1 ]
-            then
-                busybox mount -o loop /mnt/sdcard/apl.img /data/local/linux
-                startServices    
-                SD=0
-
-            elif [ -f /mnt/extsd/apl.img ] && [ $SD -eq 1 ]
-            then
-                busybox mount -o loop /mnt/extsd/apl.img /data/local/linux 
-                startServices
-                SD=0
-            fi
-
-    
-    while [ $(ps|busybox grep com.aakash.lab |busybox wc -l) -eq 1 ]
-    
-        do
-#        	busybox  chroot  $MNT /bin/bash -c "mkdir /proc"
-#        	busybox  chroot  $MNT /bin/bash -c "mkdir /sys"
-#        	busybox  chroot  $MNT /bin/bash -c "mkdir -p /dev/pts"
-#        	busybox  chroot  $MNT /bin/bash -c "echo 1 > /proc/sys/vm/drop_caches"
         	
+        if [ ! -d ${SDCARD} ] || [ ! -d ${WWW} ] || [ ! -d ${EG} ]
+        then                                                                  
+	    busybox mkdir -p ${SDCARD}/c/code                                         
+            busybox mkdir -p ${WWW}/c/code                                            
+            busybox mkdir -p ${EG}/c
+	    
+            busybox mkdir -p ${SDCARD}/cpp/code                                       
+            busybox mkdir -p ${WWW}/cpp/code                                          
+            busybox mkdir -p ${EG}/cpp
+	    
+            busybox mkdir -p ${SDCARD}/python/code                                     
+            busybox mkdir -p ${WWW}/python/code     
+            busybox mkdir -p ${EG}/python
+	    
+            busybox mkdir -p ${SDCARD}/scilab/code
+            busybox mkdir -p ${WWW}/scilab/code   
+            busybox mkdir -p ${EG}/scilab
 
-            if [ ! -d ${SDCARD} ] || [ ! -d ${WWW} ] || [ ! -d ${EG} ]
-   
-                then                                                                  
-	                busybox mkdir -p ${SDCARD}/c/code                                         
-            	    busybox mkdir -p ${WWW}/c/code                                            
-            	    busybox mkdir -p ${EG}/c
-
-            	    busybox mkdir -p ${SDCARD}/cpp/code                                       
-            	    busybox mkdir -p ${WWW}/cpp/code                                          
-            	    busybox mkdir -p ${EG}/cpp
-
-            	    busybox mkdir -p ${SDCARD}/python/code                                     
-            	    busybox mkdir -p ${WWW}/python/code     
-            	    busybox mkdir -p ${EG}/python
-
-            	    busybox mkdir -p ${SDCARD}/scilab/code
-            	    busybox mkdir -p ${WWW}/scilab/code   
-            	    busybox mkdir -p ${EG}/scilab
-
-            	    busybox mkdir -p ${SDCARD}/scilab/image
-            	    busybox mkdir -p ${WWW}/scilab/image   
-        	fi
+            busybox mkdir -p ${SDCARD}/scilab/image
+            busybox mkdir -p ${WWW}/scilab/image   
+        fi
+        
             
-            
-            busybox  chroot  /data/local/linux /bin/bash -c "nohup python /root/sb_manage.py &>'/dev/null'&"
+        busybox  chroot  /data/local/linux /bin/bash -c "nohup python /root/sb_manage.py &>'/dev/null'&"
+	
+        busybox mount -o bind ${SDCARD}/c/code ${WWW}/c/code
+        busybox mount -o bind ${SDCARD}/cpp/code ${WWW}/cpp/code
+        busybox mount -o bind ${SDCARD}/python/code ${WWW}/python/code
+        busybox mount -o bind ${SDCARD}/scilab/code ${WWW}/scilab/code
+        busybox mount -o bind ${SDCARD}/scilab/image ${WWW}/scilab/image
+	
+    	busybox mount -o bind ${EG}/c ${WWW}/c/exbind
+        busybox mount -o bind ${EG}/cpp ${WWW}/cpp/exbind
+        busybox mount -o bind ${EG}/python ${WWW}/python/exbind
+        busybox mount -o bind ${EG}/scilab ${WWW}/scilab/exbind
+	
+    }
 
-            busybox mount -o bind ${SDCARD}/c/code ${WWW}/c/code
-        	busybox mount -o bind ${SDCARD}/cpp/code ${WWW}/cpp/code
-        	busybox mount -o bind ${SDCARD}/python/code ${WWW}/python/code
-           	busybox mount -o bind ${SDCARD}/scilab/code ${WWW}/scilab/code
-        	busybox mount -o bind ${SDCARD}/scilab/image ${WWW}/scilab/image
+####################################################################################################
+
+
+while true
+  do
+    if [ $(mount | busybox grep /mnt/sdcard |busybox wc -l) -eq 2 ]
+    then    
+        busybox mkdir -p $MNT $MNTG $MNTI
     
-	        busybox mount -o bind ${EG}/c ${WWW}/c/exbind
-        	busybox mount -o bind ${EG}/cpp ${WWW}/cpp/exbind
-            busybox mount -o bind ${EG}/python ${WWW}/python/exbind
-           	busybox mount -o bind ${EG}/scilab ${WWW}/scilab/exbind
+        if [ -f /mnt/sdcard/apl.img ] && [ $APL -eq 1 ]
+        then
+            busybox mount -o loop /mnt/sdcard/apl.img /data/local/linux
+            startServicesAPL
+            APL=0
+	
+        elif [ -f /mnt/extsd/apl.img ] && [ $APL -eq 1 ]
+        then
+            busybox mount -o loop /mnt/extsd/apl.img /data/local/linux 
+            startServicesAPL
+            APL=0
+        fi 
+        
+	##############################################################
+
+        if [ -f /mnt/sdcard/gkaakash.img ] && [ $GK -eq 1 ]
+        then
+            # mounting essential file systems to chroot for gkaakash
+        	busybox mount -o loop /mnt/sdcard/gkaakash.img $MNTG
+            busybox mount -t proc proc $MNTG/proc
+            busybox mount -o bind /dev $MNTG/dev
+            busybox mount -t sysfs sysfs $MNTG/sys
+            busybox chroot  $MNTG /bin/bash -c "mount /dev/pts" 
+            busybox chroot $MNTG /bin/bash -c "source /root/.bashrc"
+        	#busybox chroot /data/local/gkaakash /bin/bash -c "python /root/ABTcore/abtstart &> '/dev/null' &"
+        	busybox chroot ${MNTG} /bin/bash -c "/root/ABTcore/abtstart"
+            GK=0
+	
+            elif [ -f /mnt/extsd/gkaakash.img ] && [ $GK -eq 1 ]
+            then
+            # mounting essential file systems to chroot for gkaakash
+        	busybox mount -o loop /mnt/extsd/gkaakash.img $MNTG
+            busybox mount -t proc proc $MNTG/proc
+            busybox mount -o bind /dev $MNTG/dev
+            busybox mount -t sysfs sysfs $MNTG/sys
+            busybox chroot  $MNTG /bin/bash -c "mount /dev/pts" 
+            busybox chroot $MNTG /bin/bash -c "source /root/.bashrc"
+	        #busybox chroot /data/local/gkaakash /bin/bash -c "python /root/ABTcore/abtstart &> '/dev/null' &"
+        	busybox chroot ${MNTG} /bin/bash -c "/root/ABTcore/abtstart"
+            GK=0
+        fi
+        
+	###############################################################
+
+        if [ -f /mnt/sdcard/ipy.img ] && [ $IPY -eq 1 ]
+        then
+        	busybox mount -o loop /mnt/sdcard/ipy.img $MNTI
+            busybox mount -t proc proc $MNTI/proc
+            busybox mount -o bind /dev $MNTI/dev
+            busybox mount -t sysfs sysfs $MNTI/sys
+            busybox chroot $MNTI /bin/bash -c "mount /dev/pts" 
+    	    busybox chroot $MNTI /bin/bash -c "/root/ipython/ipython.py notebook --no-mathjax --pylab=inline &"
+            IPY=0
     
-    	   	FLAG=0
-        	exit 0 
-    done
+        elif [ -f /mnt/extsd/ipy.img ] && [ $IPY -eq 1 ]
+        then
+    	    busybox mount -o loop /mnt/extsd/ipy.img $MNTI
+            busybox mount -t proc proc $MNTI/proc
+            busybox mount -o bind /dev $MNTI/dev
+            busybox mount -t sysfs sysfs $MNTI/sys
+            busybox chroot $MNTI /bin/bash -c "mount /dev/pts" 
+            busybox chroot $MNTI /bin/bash -c "/root/ipython/ipython.py notebook --no-mathjax --pylab=inline &"
+            IPY=0
+        fi
+        exit 0
+    fi
     sleep 1 
-
 done
-exit 0
